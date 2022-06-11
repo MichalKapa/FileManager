59f419a70e53c55c0a4f31ab66bcae3a80b0e73b
Michał Kapała <michal.kapala@yumasoft.pl>
2022-06-11 17:24:54 +0200

    Added file sorting into created directories.

--- pythonProjectJS/controller.py
+++ pythonProjectJS/controller.py
@@ -1,11 +1,18 @@
+import random
+
 from model import Model
 from view import View
 
 
 class Controller:
+
     def __init__(self):
         self.model = Model()
         self.view = View(self)
+        self.path = "../"
+        self.outputDirectory = "fileManagerOutput"
+        self.outputDirectoryPath = "../fileManagerOutput"
+        self.configurationPath = "../configuration"
 
     def main(self):
         self.view.main()
@@ -13,4 +20,13 @@ class Controller:
 
 if __name__ == '__main__':
     app = Controller()
+    print(app.model.createDirectory(app.path, app.outputDirectory))
+
+    app.model.createAllFolders(app.configurationPath, app.outputDirectoryPath)
+
+    # test files
+    # app.model.createTestFiles(app.configurationPath, app.outputDirectoryPath)
+
+    app.model.sortFiles(app.configurationPath, app.outputDirectoryPath)
+
     app.main()
--- pythonProjectJS/model.py
+++ pythonProjectJS/model.py
@@ -1,5 +1,95 @@
+import csv
+import os
+import random
+
 
 class Model:
 
     def __init__(self):
         pass
+
+    def getFilesInDirectory(self, path):
+        return os.listdir(path)
+
+    def createDirectory(self, path, dirName):
+        dirPath = os.path.join(path, dirName)
+        if not os.path.exists(dirPath):
+            os.mkdir(dirPath)
+            return "Created new folder: " + dirName
+
+        return "Folder " + dirName + " already exists"
+
+    def moveToDirectory(self, src, dst):
+        os.replace(src, dst)
+
+        return "Moved to: " + dst
+
+    # create test file
+    def createFile(self, path, fileName):
+        try:
+            with open(path + fileName, 'w') as f:
+                f.write('Created new file: ' + fileName)
+        except FileNotFoundError:
+            print("The " + path + " directory does not exist")
+
+    def createTestFiles(self, configurationPath, directoryPath):
+        with open(configurationPath + "/" + 'extensionFolders.csv') as fileObject:
+            readerObject = csv.reader(fileObject, delimiter=",")
+            for line in readerObject:
+                for extension in line[1].split(";"):
+                    self.createFile(directoryPath + "/", str(random.randint(0, 100)) + "." + extension)
+
+    def createAllFolders(self, configurationPath, outputPath):
+        self.createExtensionFolders(configurationPath, outputPath)
+        self.createNameFolders(configurationPath, outputPath)
+        print("All folders ready")
+
+    def createExtensionFolders(self, configurationPath, outputPath):
+        self.createFolders(configurationPath, outputPath, 'extensionFolders.csv')
+
+    def createNameFolders(self, configurationPath, outputPath):
+        self.createFolders(configurationPath, outputPath, 'nameFolders.csv')
+
+    # get folders of files distinguished by extension or name
+    def createFolders(self, path, outputPath, csvFile):
+        with open(path + "/" + csvFile) as fileObject:
+            readerObject = csv.reader(fileObject, delimiter=",")
+            for line in readerObject:
+                self.createDirectory(outputPath, line[0])
+
+    def sortFiles(self, configurationPath, outputDirectoryPath):
+        folderList = []
+        folderNameList = []
+
+        with open(configurationPath + "/" + 'nameFolders.csv') as fileObject:
+            readerObject = csv.reader(fileObject, delimiter=",")
+            for line in readerObject:
+                folderNameList.append(line)
+
+        with open(configurationPath + "/" + 'extensionFolders.csv') as fileObject:
+            readerObject = csv.reader(fileObject, delimiter=",")
+            for line in readerObject:
+                folderList.append(line)
+
+        fileList = self.getFilesInDirectory(outputDirectoryPath)
+        for file in fileList:
+            if not os.path.isdir(outputDirectoryPath + "/" + file):
+                for folder in folderNameList:
+                    if folder[1].lower() in file.lower():
+                        print(self.moveToDirectory(outputDirectoryPath + "/" + file,
+                                                   outputDirectoryPath + "/" + folder[0] + "/" + file))
+                        break
+
+            fileList = self.getFilesInDirectory(outputDirectoryPath)
+        for file in fileList:
+            fileSorted = False
+            for folder in folderList:
+                if fileSorted:
+                    break
+
+                for extension in folder[1].split(";"):
+                    if "." + extension in file:
+                        self.moveToDirectory(outputDirectoryPath + "/" + file,
+                                                   outputDirectoryPath + "/" + folder[0] + "/" + file)
+                        fileSorted = True
+                        break
